4fdd5b9ba8208ae23b3d698cf3088606df91ec05
16cats <97065659+16cats@users.noreply.github.com>
2024-09-25 15:48:02 +0300

    LuoKentta();

--- Pong/Pong.cs
+++ Pong/Pong.cs
@@ -17,8 +17,26 @@ public class Pong : PhysicsGame
     public override void Begin()
     {
         PhysicsObject pallo = new PhysicsObject(40.0, 40.0, Shape.Circle);
+        pallo.X = -200.0;
+        pallo.Y = 0.0;
+        pallo.Restitution = 1.0;
+
         Add(pallo);
+        
+        LuoKentta();
+        
+        Vector impulssi = new Vector(500.0, 0.0);
+        pallo.Hit(impulssi * pallo.Mass);
         
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
     }
+
+    private void LuoKentta()
+    {
+        Level.CreateBorders(1.0, false);
+        Level.Background.Color = Color.Black;
+        
+        Camera.ZoomToLevel();
+    }
+    
 }
