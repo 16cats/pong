65fce2758eec5b6aefcdd64ba3c7971722dd6d88
16cats <97065659+16cats@users.noreply.github.com>
2024-09-25 16:00:41 +0300

    AloitaPeli();

--- Pong/Pong.cs
+++ Pong/Pong.cs
@@ -16,27 +16,44 @@ public class Pong : PhysicsGame
 {
     public override void Begin()
     {
-        PhysicsObject pallo = new PhysicsObject(40.0, 40.0, Shape.Circle);
-        pallo.X = -200.0;
-        pallo.Y = 0.0;
-        pallo.Restitution = 1.0;
-
-        Add(pallo);
+        PhysicsObject pallo = LuoPallo(this, -200, 0);
         
         LuoKentta();
-        
-        Vector impulssi = new Vector(500.0, 0.0);
-        pallo.Hit(impulssi * pallo.Mass);
+        AloitaPeli(pallo);
         
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
     }
 
     private void LuoKentta()
     {
+        PhysicsObject maila = PhysicsObject.CreateStaticObject(20.0, 100.0, Shape.Rectangle);
+        maila.X = Level.Left + 20.0;
+        maila.Y = 0.0;
+        maila.Restitution = 1.0;
+        Add(maila);
+        
         Level.CreateBorders(1.0, false);
         Level.Background.Color = Color.Black;
         
         Camera.ZoomToLevel();
+    }
+    
+    public static PhysicsObject LuoPallo(PhysicsGame peli, double x, double y)
+    {
+        PhysicsObject pallo = new PhysicsObject(40.0, 40.0, Shape.Circle);
+        pallo.X = x;
+        pallo.Y = y;
+        pallo.Restitution = 1.0;
+        pallo.KineticFriction = 0.0;
+        pallo.MomentOfInertia = double.PositiveInfinity;
+        peli.Add(pallo);
+        return pallo;
+    }
+    
+    private static void AloitaPeli(PhysicsObject pallo)
+    {
+        Vector impulssi = new Vector(500.0, 0.0);
+        pallo.Hit(impulssi * pallo.Mass);
     }
     
 }
